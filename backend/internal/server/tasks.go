4d334e191cb8920bd56ad4fe9b762e187d878122
Aleksander Gencel <johnnylittleplanet@gmail.com>
2025-05-17 01:28:29 +0200

    backend: Add tasktypes endpoint

--- backend/internal/server/tasks.go
+++ backend/internal/server/tasks.go
@@ -1,7 +1,22 @@
 package server
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"hackathon-project/internal/database"
+	"hackathon-project/internal/models"
+	"net/http"
+)
 
 func HandlerGetTasks(c *gin.Context) {
 	c.Status(200)
+}
+
+func HandlerGetAllTaskTypes(c *gin.Context) {
+	var taskTypes []models.TaskType
+	response := database.DB.Find(&taskTypes)
+	if response.Error != nil {
+		c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"message": response.Error.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, taskTypes)
 }
