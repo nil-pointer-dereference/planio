b0f80f682b076c0594b4ca30b9fab96fc318cd00
heshsaih <heshsaih@gmail.com>
2025-05-16 19:30:52 +0200

    feat: navbar and default layout implemented

--- frontend/src/layouts/DefaultLayout.tsx
+++ frontend/src/layouts/DefaultLayout.tsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router";
+import Navbar from "./navbar";
 
 export default function DefaultLayout() {
   return (
-    <div>
-      <Outlet></Outlet>
-    </div>
+    <>
+      <Navbar></Navbar>
+      <div className="w-full h-screen flex flex-col justify-top items-center pl-30 pr-30 pt-25">
+        <Outlet></Outlet>
+      </div>
+    </>
   );
 }
