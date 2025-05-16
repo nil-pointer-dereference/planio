570c2537372df60ad5e18374812beb26cf9cb3b0
TheGroobi <23wilk.szymon@gmail.com>
2025-05-16 22:27:26 +0200

    feature(seed): create user and task seeds

---
+++ backend/internal/seed/task.go
@@ -0,0 +1,27 @@
+package seed
+
+import (
+	"hackathon-project/internal/models"
+)
+
+var TaskTypes = []models.TaskType{
+	{Type: "Work"},
+	{Type: "Family"},
+	{Type: "Hobby"},
+	{Type: "Training"},
+	{Type: "Mental Health"},
+	{Type: "Fitness"},
+	{Type: "Shopping"},
+	{Type: "Finance"},
+	{Type: "Social"},
+	{Type: "Education"},
+	{Type: "Chores"},
+	{Type: "Health"},
+	{Type: "Travel"},
+	{Type: "Personal Development"},
+	{Type: "Urgent"},
+	{Type: "Leisure"},
+	{Type: "Errands"},
+	{Type: "Meetings"},
+	{Type: "Creative"},
+}
