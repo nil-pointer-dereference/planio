c9190d4f66376caef62d65956e9183fa88f644a1
Kacper Majkowski <kacpermajkowski@proton.me>
2025-05-16 14:54:23 +0200

    configured TailwindCSS and SchadCN

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from '@tailwindcss/vite';
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
+  // SchadCN config
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  // end of SchadCN config
   server: {
     proxy: {
       "/api": {
