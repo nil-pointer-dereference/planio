820afa36773e9f40f88e7d29daffb1bb36fd17c1
heshsaih <heshsaih@gmail.com>
2025-05-17 02:36:00 +0200

    feat: questionnaire hook, loading screen, auth hook implementations

---
+++ frontend/src/api/config.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://localhost:3000",
+});
+
+api.interceptors.request.use(
+  (cfg) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      cfg.headers.Authorization = `Bearer ${token}`;
+    }
+    return cfg;
+  },
+  (err) => Promise.reject(err),
+);
+
+export default api;
