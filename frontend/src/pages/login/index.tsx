23267872683074ecc7b4aa7963bda245821bbc62
heshsaih <heshsaih@gmail.com>
2025-05-17 02:57:23 +0200

    feat: questionnaire hook, loading screen, auth hook implementations

--- frontend/src/pages/login/index.tsx
+++ frontend/src/pages/login/index.tsx
@@ -1,8 +1,67 @@
-import { LoginForm } from "@/components/login-form";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "react-day-picker";
+import { Form, useForm } from "react-hook-form";
+import { z } from "zod";
+
+const loginSchema = z.object({
+  username: z.string(),
+  password: z.string(),
+});
+
+type LoginSchema = z.infer<typeof loginSchema>;
+
 export default function LoginPage() {
+  const a = useForm<LoginSchema>({
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
+
+  const handleSubmit = a.handleSubmit((fields) => {
+    console.log(fields);
+  });
+
   return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-      <LoginForm />
+    <div className="flex flex-col w-full justify-center items-center">
+      <Card>
+        <CardHeader>
+          <CardTitle>Tytuł</CardTitle>
+          <CardDescription>Opis</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Form {...a}>
+            <form onSubmit={handleSubmit}>
+              <FormField
+                control={a.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Nazwa użytkownika</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Nazwa użytkownika" {...field}></Input>
+                    </FormControl>
+                  </FormItem>
+                )}
+              ></FormField>
+              <Button type="submit">Zaloguj się</Button>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
     </div>
   );
 }
