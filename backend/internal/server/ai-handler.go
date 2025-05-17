162eb83c7aa7d24b36d62ab69cd03cc9303ecb3a
Aleksander Gencel <johnnylittleplanet@gmail.com>
2025-05-17 04:34:18 +0200

    backend: Personalise even further AI prompt for tasks

--- backend/internal/server/ai-handler.go
+++ backend/internal/server/ai-handler.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"encoding/json"
+	"fmt"
 	"hackathon-project/internal/database"
 	"hackathon-project/internal/models"
 	"net/http"
@@ -22,29 +23,53 @@ func HandlerPostAI(c *gin.Context) {
 		c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
 		return
 	}
+	var taskTypes []models.TaskType
+	response := database.DB.Find(&taskTypes)
+	if response.Error != nil {
+		fmt.Println("Continuing on empty taskTypes context: " + response.Error.Error())
+	}
+	jsonTypes, _ := json.Marshal(taskTypes)
+	fmt.Println(string(jsonTypes))
+
+	var tasks []models.Task
+	response = database.DB.Find(&tasks)
+	if response.Error != nil {
+		fmt.Println("Continuing on empty tasks: " + response.Error.Error())
+	}
+	jsonTasks, _ := json.Marshal(tasks)
+	fmt.Println(string(jsonTasks))
+
 	aiCtx := CreateNewAIContext(
 		map[string]string{
-			"Name":                      userFromSession.Name,
-			"Age":                       userFromSession.Birthday,
-			"Interests":                 userForm.Interests,
-			"Goes to work?":             strconv.FormatBool(userForm.DoesWork),
-			"Goals":                     userForm.Goals,
-			"Free time activities":      userForm.FreeTimeActivites,
-			"Ways to rest":              userForm.Rest,
-			"Ways for entertain":        userForm.Rest,
-			"Level of stress 1-5:":      string(userForm.LevelOfStress),
-			"Intensive days 1-5:":       string(userForm.DayIntensiveness),
-			"Experience in sports 1-5:": string(userForm.SportsExperience),
-			"Mandatory wake up time:":   userForm.WakeUpTime,
-			"Sleep hours":               string(userForm.SleepHours),
+			"Name":                         userFromSession.Name,
+			"Age":                          userFromSession.Birthday,
+			"Interests":                    userForm.Interests,
+			"Goes to work?":                strconv.FormatBool(userForm.DoesWork),
+			"Goals":                        userForm.Goals,
+			"Free time activities":         userForm.FreeTimeActivites,
+			"Ways to rest":                 userForm.Rest,
+			"Ways for entertain":           userForm.Entertainment,
+			"Level of stress 1-5:":         string(userForm.LevelOfStress),
+			"Intensive days 1-5:":          string(userForm.DayIntensiveness),
+			"Experience in sports 1-5:":    string(userForm.SportsExperience),
+			"Mandatory wake up time:":      userForm.WakeUpTime,
+			"Sleep hours":                  string(userForm.SleepHours),
+			"Only Task categories to use:": string(jsonTypes),
+			"Previous tasks and their summaries to learn:": string(jsonTasks),
+			"Work shift time:": "8 am to 4 pm",
 		},
 		&[]models.Task{}, // empty notes
 		"raw generated tasks in json, do not wrap the json codes in JSON markdown markers. do not append any data from me."+
 			"You have 24 hour day. You have user data. task format: Type string, Completed bool, Title (max 4 words) string, Description (max 2 sentences) string EstimatedMinutes int."+
 			"if you consider sports, mind Experience in sports. Tasks should have start hour and mind their estimatedminutes."+
-			"You can adjust sleep time +/- 1 hour."+
+			"You can adjust sleep time +/- 1 hour. If you do, tell it in the description of sleep."+
 			"Only one sleep session. Think about it as when to go to sleep and start other tasks after the sleep task period."+
-			"Do not add commute time, add a little bit to task duration which needs commuting.",
+			"Do not add commute time, add a little bit to task duration which needs commuting."+
+			"Focus on task summaries if they exist."+
+			"No tasks should overlap. Meaning that start hour + estimated minutes sum should not overflow on other task's start hour + estimated minutes, too."+
+			"Work shift time should always be in the same time, no matter what. Meaning that task of Type 'Work' can be only one within the designated hours and no more."+
+			"Do not mention any medication or things that do not require doctor's diagnosis."+
+			"Try to make tasks lightweight.",
 	)
 
 	raw, err := aiCtx.CreateMsg().WithFormatting().WithTasks().RunPrompt(c)
