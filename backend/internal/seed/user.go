910441dad8da81ce919f827c3794d5546882b55c
TheGroobi <23wilk.szymon@gmail.com>
2025-05-17 04:36:14 +0200

    update seed

--- backend/internal/seed/user.go
+++ backend/internal/seed/user.go
@@ -3,10 +3,10 @@ package seed
 import "hackathon-project/internal/models"
 
 var Users = []models.User{
-	{Username: "alice123", Password: "password1", Name: "Alice Johnson", Birthday: "1990-05-15"},
-	{Username: "bob_smith", Password: "secretpass", Name: "Bob Smith", Birthday: "1985-10-30"},
-	{Username: "charlie77", Password: "mypassword", Name: "Charlie Brown", Birthday: "1992-12-01"},
-	{Username: "diana.k", Password: "diana1234", Name: "Diana King", Birthday: "1988-07-22"},
-	{Username: "eric_w", Password: "ericpass", Name: "Eric White", Birthday: "1995-03-10"},
-	{Username: "test", Password: "123", Name: "Giga Sigma", Birthday: "1992-01-12"},
+	{Id: 1, Username: "alice123", Password: "password1", Name: "Alice Johnson", Birthday: "1990-05-15"},
+	{Id: 2, Username: "bob_smith", Password: "secretpass", Name: "Bob Smith", Birthday: "1985-10-30"},
+	{Id: 3, Username: "charlie77", Password: "mypassword", Name: "Charlie Brown", Birthday: "1992-12-01"},
+	{Id: 4, Username: "diana.k", Password: "diana1234", Name: "Diana King", Birthday: "1988-07-22"},
+	{Id: 5, Username: "eric_w", Password: "ericpass", Name: "Eric White", Birthday: "1995-03-10"},
+	{Id: 6, Username: "test", Password: "123", Name: "Giga Sigma", Birthday: "1992-01-12"},
 }
