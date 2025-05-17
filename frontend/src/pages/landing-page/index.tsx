b2b3718a75dcc9b87469618debc0acd3875c1270
TheGroobi <23wilk.szymon@gmail.com>
2025-05-17 02:42:19 +0200

    update landing page

--- frontend/src/pages/landing-page/index.tsx
+++ frontend/src/pages/landing-page/index.tsx
@@ -1,30 +1,34 @@
 import { Button } from "@/components/ui/button";
+import { ArrowUpRight, BotMessageSquare } from "lucide-react";
 import { Link } from "react-router";
 
 export default function LandingPage() {
   return (
-      <div className="w-full h-full bg-radial-[at_5%_100%] from-background to-primary to-90% gap-10 flex flex-col justify-center items-center pl-10 pr-10">
-        <div className="w-full flex flex-col justify-start items-center gap-8">
-          <h1 className="lg:text-[20rem] text-9xl text-center fluid-text-animation ">
-            WIP
-          </h1>
-          <h1 className="lg:text-3xl text-lg text-wrap text-center w-3/4 ">
-            Nasza Aplikacja jest bardzo fajna i na pewno będzie pomocna w
-            rozwiązywaniu twoich problemów psychicznych. Tak o bez kitu serio.{" "}
-            <span className="text-secondary/90">
-              Gdzie są dwa sosy czosnkowe?
-            </span>{" "}
-            Mówie 100% bez beki Gdzie są dwa sosy czosnkowe?
-          </h1>
-        </div>
-        <Link className="w-1/3 min-w-3xs h-1/12" to="/login">
-          <Button
-            className="w-full h-full lg:text-3xl bg-linear-to-l from-background/40 to-primary/60 hover:from-primary/80 hover:to-accent/80 hover:border-2 hover:border-primary hover:text-background rounded-4xl"
-            variant="outline"
-          >
-            Get Started
-          </Button>
-        </Link>
+    <div className="max-w-full h-screen bg-background gap-8 flex flex-col justify-center items-center">
+      <div className="absolute rounded-full opacity/5 -top-1/2 -translate-x-1/2 left-1/2 w-[10rem] h-[50rem] rotate-60 bg-primary blur-[16rem] z-100"></div>
+      <div className="w-full flex flex-col justify-start items-center gap-12">
+        <h1 className="lg:text-[10rem] text-7xl text-center flex items-center gap-6 fluid-text-animation group">
+          <BotMessageSquare className="inline text-primary size-[9.5rem] pb-4" />
+          Planio{" "}
+        </h1>
+        <h1 className="lg:text-2xl !text-xl text-muted-foreground text-wrap text-center w-3/4 !font-['Poppins']">
+          Planio to inteligentny asystent planowania, który automatycznie
+          dostosowuje Twój harmonogram do zmieniających się potrzeb i wykonanych
+          zadań.
+        </h1>
       </div>
+      <Link
+        to="/login"
+        className="w-full items-center flex group justify-center"
+      >
+        <Button
+          className="relative rounded-full text-primary border-primary text-2xl !px-6 !py-6 bg-opacity-0 bg-[radial-gradient(circle,_rgba(170,121,246,0.4)_0%,_transparent_100%)] bg-no-repeat bg-center bg-[length:0px_100%] hover:bg-[length:100%_100%] hover:bg-transparent hover:text-primary hover:!px-8 transition-all duration-300"
+          variant="outline"
+        >
+          Zaplaniuj swój dzień
+          <ArrowUpRight className="size-6" />
+        </Button>
+      </Link>
+    </div>
   );
 }
