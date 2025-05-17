3721d98b199295512d18b42a273360e4d358bd8e
BartekRutowicz <bartirut@gmail.com>
2025-05-17 11:05:28 +0200

    Fixed input buttons

--- frontend/src/components/AiPopup/AiPopup.tsx
+++ frontend/src/components/AiPopup/AiPopup.tsx
@@ -5,16 +5,20 @@ import {
 } from "@radix-ui/react-popover";
 import PopupInput from "./PopupInput";
 import { useState } from "react";
+import { LucideFileEdit } from "lucide-react";
 import ResponseWindow from "./ResponseWindow";
 
 export default function AiPopup() {
   const [open, setOpen] = useState(false);
 
   return (
-    <div className="absolute max-w-lg right-50 top-20">
+    <div className="fixed max-w-lg right-20 top-25 z-50">
       <Popover open={open} onOpenChange={setOpen}>
-        <PopoverTrigger className="cursor-pointer bg-accent p-3 rounded-lg hover:bg-linear-to-l from-background/40 to-accent-foreground/60 hover:from-accent-foreground/80 hover:to-accent/80 hover:text-background">
-          Manage Task
+        <PopoverTrigger className="cursor-pointer lg:text-sm bg-accent p-1 rounded-lg hover:bg-linear-to-l from-background/40 to-accent-foreground/60 hover:from-accent-foreground/80 hover:to-accent/80 hover:text-background">
+          <div className="flex justify-center items-center text-2xl gap-3 p-3">
+            <LucideFileEdit></LucideFileEdit>
+            Zarządzaj Zadaniem
+          </div>
         </PopoverTrigger>
         <PopoverContent sideOffset={15} className="animate-fade-in max-w-lg">
           <PopupInput setOpenParent={setOpen}></PopupInput>
