c40ac0e532b32775e8cb8a38add344970a2b4eb2
TheGroobi <23wilk.szymon@gmail.com>
2025-05-17 02:46:22 +0200

    quick fix navbar route

--- frontend/src/layouts/DefaultLayout.tsx
+++ frontend/src/layouts/DefaultLayout.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router";
 import AppBar from "@/components/appbar";
-import Navbar from "./Navbar";
+import Navbar from "./navbar";
 
 export default function DefaultLayout() {
   return (
